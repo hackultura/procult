5540d3a3380ae520377a87e8f38d080810c330b8
gilsondev <me@gilsondev.in>
2016-02-04 16:28:58 -0200

    Inserindo validacao de CPF e CNPJ

--- procult/authentication/serializers.py
+++ procult/authentication/serializers.py
@@ -2,10 +2,13 @@
 
 from django.contrib.auth import update_session_auth_hash
 from rest_framework import serializers
+from rest_localflavor.br.serializers import BRCPFField, BRCNPJField
 from .models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
+    cpf = BRCPFField(allow_blank=True)
+    cnpj = BRCNPJField(allow_blank=True)
     password1 = serializers.CharField(write_only=True, required=False)
     password2 = serializers.CharField(write_only=True, required=False)
 
@@ -14,6 +17,13 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ('id', 'email', 'name', 'cpf', 'cnpj', 'ceac',
                   'password1', 'password2', 'is_admin',)
         read_only_fields = ('created_at', 'updated_at', 'is_admin')
+
+    def validate(self, data):
+        import ipdb; ipdb.set_trace()
+        if data['cpf'] in ['', None] and data['cnpj'] in ['', None]:
+            raise serializers.ValidationError("Número do seu documento é obrigatório")
+
+        return data
 
     def create(self, validated_data):
         password1 = validated_data.pop('password1', None)
@@ -25,7 +35,7 @@ class UserSerializer(serializers.ModelSerializer):
             user.save()
         else:
             raise serializers.ValidationError(
-                {'password2', 'As senhas não se batem. Digite novamente'}
+                "As senhas não se batem. Digite novamente"
             )
 
         return user
