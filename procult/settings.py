3b093952ea1de4c91ed419eceadb351676bc9928
André decko de Brito <decko@birosca.mobi>
2016-02-17 10:59:20 -0200

    Corrige a construção do ALLOWED_HOSTS, obtendo a variavel de ambiente e separando os valores por virgula

--- procult/settings.py
+++ procult/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.getenv(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', True)
 
-ALLOWED_HOSTS = [os.getenv('ALLOWED_DOMAIN', 'localhost')]
+ALLOWED_HOSTS = os.getenv('ALLOWED_DOMAIN', 'localhost').split(',')
 
 
 # Application definition
