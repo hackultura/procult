d181e5f447f57f790e293ed82c97ef2eea207c23
gilsondev <me@gilsondev.in>
2016-03-08 13:34:01 -0300

    Inserindo migracao para mudar ordenamento para data de criacao e atualizacao

---
+++ procult/core/migrations/0002_auto_20160308_1329.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='proposal',
+            options={'ordering': ['-created_at', '-updated_at']},
+        ),
+    ]
