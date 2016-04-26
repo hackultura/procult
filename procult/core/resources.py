2f25b2eb0e0ec51b1a824ac852c2b8a26138d793
gilsondev <me@gilsondev.in>
2016-04-26 15:00:40 -0300

    Inserindo campo de documento CPF ou CNPJ

--- procult/core/resources.py
+++ procult/core/resources.py
@@ -12,16 +12,17 @@ class ProposalResource(resources.ModelResource):
     numero = fields.Field()
     pasta_proposta = fields.Field()
     artista = fields.Field()
+    documento = fieldsField()
     projeto = fields.Field()
     criado_em = fields.Field()
     enviado_em = fields.Field()
 
     class Meta:
         model = Proposal
-        fields = ("numero", "pasta_proposta", "artista", "projeto",
+        fields = ("numero", "pasta_proposta", "artista", "documento", "projeto",
                   "criado_em", "enviado_em",)
-        export_order = ("numero", "pasta_proposta", "artista", "projeto",
-                        "criado_em", "enviado_em",)
+        export_order = ("numero", "pasta_proposta", "artista","projeto",
+                        "documento", "criado_em", "enviado_em",)
         exclude = ("id", "number", "ente", "title",
                    "status", "created_at", "sended_at", "updated_at",)
 
@@ -36,6 +37,9 @@ class ProposalResource(resources.ModelResource):
 
     def dehydrate_artista(self, proposal):
         return proposal.ente.user.name
+
+    def dehydrate_documento(self, proposal):
+        return proposal.ente.cpf or proposal.ente.cnpj
 
     def dehydrate_projeto(self, proposal):
         return proposal.title
