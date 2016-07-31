16294fad99f3c7ac02819f04b4932e657b21f84e
Gustavo Jaruga Cruz <darksshades@hotmail.com>
2016-07-31 13:06:27 -0300

    Add valitation to user, fix password input
    Signed-off-by: Gustavo Jaruga Cruz <darksshades@gmail.com>

--- procult/authentication/admin.py
+++ procult/authentication/admin.py
@@ -1,10 +1,32 @@
+# -*- coding: utf-8 -*-
+
 from django.contrib import admin
+import django.contrib.auth.admin as admin_auth
 from .models import User, Ente
 from .forms import EnteForm
+from django.utils.translation import ugettext_lazy as _
 
 
-class UserAdmin(admin.ModelAdmin):
-    pass
+class UserAdmin(admin_auth.UserAdmin):
+    readonly_fields = ('created_at', 'updated_at')
+    fieldsets = (
+        (None, {'fields': ('email', 'password', 'is_admin')}),
+        (_('Dados pessoais'), {'fields': ('name', )}),
+        (_('Datas'), {'fields': ('created_at', 'updated_at')}),
+        (_('Permissoes'), {'fields': ('is_superuser',
+                                      'groups', 'user_permissions')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('name', 'email', 'password1', 'password2',
+                       'is_admin', 'groups')}
+         ),
+    )
+    list_filter = []
+    list_display = ('email', 'name')
+    search_fields = ('email', 'name')
+    ordering = ('email',)
 
 
 class EnteAdmin(admin.ModelAdmin):
